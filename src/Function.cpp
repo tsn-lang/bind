e30aa00c9104b3274c9a9d1d00fe542fd323ecb9
Michael DeCicco <mdecicco8@gmail.com>
2024-09-02 07:21:06 -0400

    add `call` helper to `Function`

--- src/Function.cpp
+++ src/Function.cpp
@@ -1,4 +1,6 @@
 #include <bind/Function.h>
+#include <bind/interfaces/ICallHandler.h>
+#include <utils/Exception.h>
 
 namespace bind {
     Function::Function(const String& name, FunctionType* sig, Namespace* ns)
@@ -17,6 +19,14 @@ namespace bind {
 
     void Function::setCallHandler(ICallHandler* callHandler) {
         m_callHandler = callHandler;
+    }
+    
+    void Function::call(void* retDest, void** args) {
+        if (!m_callHandler) {
+            throw Exception("Function::call - function '%s' has no call handler", getFullName().c_str());
+        }
+
+        m_callHandler->call(this, retDest, args);
     }
 
     FunctionType* Function::getSignature() const {
